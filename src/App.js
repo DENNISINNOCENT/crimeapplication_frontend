310035e6a3e9ab65817d5e5e10e7be414e54b7c1
DENNISINNOCENT <mutindahdennis@gmail.com>
2022-09-08 22:48:28 +0300

    importing my components to app.js

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React  from 'react'
+
+import CrimeList from './components/CrimeList'
+import ReportCrime from "./components/ReportCrime"
+
+
+
+function App() {
+  
+ 
+  
+
+
+  return (
+    
+    <div className='App'> 
+ 
+     <CrimeList />
+     <ReportCrime/>
+     
+     </div>
+     
+    
+    
+      
+    
+  )
+}
+
+export default App
